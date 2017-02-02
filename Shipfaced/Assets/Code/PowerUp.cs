2c76e0d1184df01d4dbe2c951eb4f6899caa1dab
Andreas Halldin <iqed@live.se>
2017-02-02 14:46:33 +0100

    Power Ups V1 + Boat controls

---
+++ Shipfaced/Assets/Code/PowerUp.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject playerManager;
+    List<Action> powerUps = new List<Action>();
+    int activatingPlayer;
+    void Awake()
+    {
+        playerManager = GameObject.Find("PlayerManager");
+        powerUps.Add(PowerUpShuffleOthers);
+        powerUps.Add(PowerUpSwitchOthers);
+        // powerUps.Add(PowerUpSlowOthers);
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        for (int i = 0; i < playerManager.GetComponent<PlayerManager>().players.Length; i++)
+        {
+            if (c.gameObject.transform.parent.parent.gameObject.GetComponent<SimpleCarController>() == playerManager.GetComponent<PlayerManager>().players[i])
+            {
+                activatingPlayer = i;
+                break;
+            }
+        }
+        Destroy(gameObject);
+        powerUps[UnityEngine.Random.Range(0, powerUps.Count)]();
+    }
+
+    //PowerUps
+    //UI för PowerUps
+
+    void PowerUpShuffleOthers()
+    {
+        for (int i = 0; i < playerManager.GetComponent<PlayerManager>().players.Length; i++)
+        {
+            if (i == activatingPlayer)
+            {
+                continue;
+            }
+            playerManager.GetComponent<PlayerManager>().players[i].ShuffleKeys();
+            playerManager.GetComponent<PlayerManager>().textList[i].text = "P" + (i + 1) + ": " + playerManager.GetComponent<PlayerManager>().players[i].leftKey.ToString() + " || " + playerManager.GetComponent<PlayerManager>().players[i].rightKey.ToString();
+        }
+        print("Shuffle!");
+    }
+
+    void PowerUpSwitchOthers()
+    {
+        for (int i = 0; i < playerManager.GetComponent<PlayerManager>().players.Length; i++)
+        {
+            if (i == activatingPlayer)
+            {
+                continue;
+            }
+            KeyCode temp;
+            temp = playerManager.GetComponent<PlayerManager>().players[i].leftKey;
+            playerManager.GetComponent<PlayerManager>().players[i].leftKey = playerManager.GetComponent<PlayerManager>().players[i].rightKey;
+            playerManager.GetComponent<PlayerManager>().players[i].rightKey = temp;
+            playerManager.GetComponent<PlayerManager>().textList[i].text = "P" + (i + 1) + ": " + playerManager.GetComponent<PlayerManager>().players[i].leftKey.ToString() + " || " + playerManager.GetComponent<PlayerManager>().players[i].rightKey.ToString();
+        }
+        print("Switch!");
+    }
+
+    void PowerUpSlowOthers()
+    {
+        for (int i = 0; i < playerManager.GetComponent<PlayerManager>().players.Length; i++)
+        {
+            if (i == activatingPlayer)
+            {
+                continue;
+            }
+
+            StartCoroutine(SlowDown(playerManager.GetComponent<PlayerManager>().players[i].gameObject.GetComponent<Rigidbody>()));
+
+        }
+        print("Slow Down!");
+    }
+
+    IEnumerator SlowDown(Rigidbody boat)
+    {
+        //Slowdown
+        yield return new WaitForSeconds(1f);
+        //return
+    }
+
+}
---
+++ Shipfaced/Assets/Code/SpawnPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPowerUp : MonoBehaviour
+{
+    public GameObject powerup;
+    public List<GameObject> spawnAreas = new List<GameObject>();
+    void Start()
+    {
+        Spawn();
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        GameObject spawnArea = spawnAreas[Random.Range(0, spawnAreas.Count)];
+        Instantiate(powerup, new Vector3(Random.Range(spawnArea.GetComponent<BoxCollider>().bounds.min.x, spawnArea.GetComponent<BoxCollider>().bounds.max.x), 0.5f, Random.Range(spawnArea.GetComponent<BoxCollider>().bounds.min.z, spawnArea.GetComponent<BoxCollider>().bounds.max.z)), Quaternion.identity);
+    }
+}
