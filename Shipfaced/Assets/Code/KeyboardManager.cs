02fa85fe9737b009fd3eb72efde2afad181446b6
Andreas Halldin <iqed@live.se>
2017-02-08 13:16:43 +0100

    Added Keyboard Prefab

---
+++ Shipfaced/Assets/Code/KeyboardManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public List<KeyCode> keys = new List<KeyCode>();
+
+    void Start()
+    {
+        UpdateKeys();
+    }
+
+    public void UpdateKeys()
+    {
+        keys.Clear();
+        foreach (UnityEngine.UI.Toggle toggle in GetComponentsInChildren<UnityEngine.UI.Toggle>())
+        {
+            if (toggle.isOn)
+            {
+                keys.Add(toggle.gameObject.GetComponent<Key>().keyCode);
+            }
+        }
+    }
+
+    
+}
