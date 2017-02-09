cfdbb554eeee7cfebb11c8bc131833a95f4aabd5
Andreas Halldin <iqed@live.se>
2017-02-09 12:12:57 +0100

    Github Pls 2.0

---
+++ Shipfaced/Assets/Code/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	AudioSource audio;
+	[SerializeField]
+	AudioClip clip;
+
+	[SerializeField]
+	float startTime = 0;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+
+		audio.time = startTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PlayClip()
+	{
+		audio.clip = clip;
+		audio.Play();
+	}
+}
